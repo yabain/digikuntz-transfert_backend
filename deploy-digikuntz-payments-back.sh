1698391188808be82278cdb140bb8fde2e3c1250
Flambel SANOU <98210672+FlambelSANOU@users.noreply.github.com>
2025-10-05 16:58:53 +0100

    .

---
+++ deploy-digikuntz-payments-back.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Backend
+
+# git clone https://github.com/yabain/digikuntz-transfert_backend.git .
+
+cd ~/public_html/app.digikuntz.com # /home/digikuntz/public_html/app.digikuntz.com en sudo su
+git pull origin main
+/opt/cpanel/ea-nodejs18/bin/npm install -f
+/opt/cpanel/ea-nodejs18/bin/npm run build
+pm2 restart digikuntz-backend --update-env # ou /opt/cpanel/ea-nodejs18/bin/pm2 start dist/main.js --name digikuntz-backend si le process n'est pas démarré
