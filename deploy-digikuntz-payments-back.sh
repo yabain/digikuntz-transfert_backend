bb75bf667b8ff000b06e38701abe700217f3defa
Flambel SANOU <98210672+FlambelSANOU@users.noreply.github.com>
2025-10-06 13:01:17 +0100

    deployment file updated

--- deploy-digikuntz-payments-back.sh
+++ deploy-digikuntz-payments-back.sh
@@ -4,8 +4,15 @@
 
 # git clone https://github.com/yabain/digikuntz-transfert_backend.git .
 
-cd ~/public_html/app.digikuntz.com # /home/digikuntz/public_html/app.digikuntz.com en sudo su
+# cd ~/public_html/app.digikuntz.com # /home/digikuntz/public_html/app.digikuntz.com en sudo su
+# git pull origin main
+# /opt/cpanel/ea-nodejs18/bin/npm install -f
+# /opt/cpanel/ea-nodejs18/bin/npm run build
+# pm2 restart digikuntz-backend --update-env # ou /opt/cpanel/ea-nodejs18/bin/pm2 start dist/main.js --name digikuntz-backend si le process n'est pas démarré
+
+cd ~/public_html/app.digikuntz.com
 git pull origin main
-/opt/cpanel/ea-nodejs18/bin/npm install -f
+/opt/cpanel/ea-nodejs18/bin/npm install
 /opt/cpanel/ea-nodejs18/bin/npm run build
-pm2 restart digikuntz-backend --update-env # ou /opt/cpanel/ea-nodejs18/bin/pm2 start dist/main.js --name digikuntz-backend si le process n'est pas démarré
+/opt/cpanel/ea-nodejs18/bin/pm2 start dist/main.js --interpreter /opt/cpanel/ea-nodejs18/bin/node --name digikuntz-backend
+pm2 save
